3c24ae3785227cb68a6143924391167c9ab983d2
Nathan <rgraves.math@gmail.com>
2017-11-11 17:01:26 -0600

    Add App Calculator Loop

--- src/appcontrolcalculator/AppControlCalculator.java
+++ src/appcontrolcalculator/AppControlCalculator.java
@@ -5,10 +5,13 @@ public class AppControlCalculator {
 
 	public static void main(String[] args) {
 		try{
+                    Boolean continueBool;
+                    do {                        
 			Scanner input = new Scanner(System.in);
 			Controller calculate = new Controller();
 			Integer input1;
 			Integer input2;
+                        
 		
 			String operator;
 		
@@ -24,7 +27,18 @@ public class AppControlCalculator {
 			System.out.println("The result is");
 		
 			calculate.handleOperator(operator,input1,input2);	
-		
+                        
+                        //Inserted for multiple runs, however didn't go through all
+                        //of the exceptions yet.
+                        System.out.print("Would you like to continue? (1 for yes anything else for no.)");
+                        input1 = Integer.parseInt(input.nextLine());
+                        if (input1 == 1){
+                            continueBool = true;
+                        } else {
+                            continueBool = false;
+                        }  
+                    } while (continueBool);
+                    
 		}catch (NumberFormatException ex) {
 			System.out.println(" ");
 			System.out.println("Please Enter Valid Numbers");
